94658f0dfc258c3fa524bdb5e6aee2984619443c
pchristensenB <38443139+pchristensenB@users.noreply.github.com>
2022-10-19 10:40:50 +0100

    okta updates

--- public/js/okta1.js
+++ public/js/okta1.js
@@ -3,11 +3,24 @@ $(document).ready(function () {
     $("#myModal").toggle();
     $("#imagemodal").toggle();
     $("#popreg").on("click", function () {
+        $('#license').hide();
+        $('#imagepreview').show();
         $('#imagepreview').attr('src', '/img/register.png');
         $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
     });
     $("#poplog").on("click", function () {
+        $('#license').hide();
+        $('#imagepreview').show();
         $('#imagepreview').attr('src', '/img/login.png');
+        $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+    });
+    $("#lic").on("click", function () {
+        $('#license').show();
+        $('#imagepreview').hide();
+        $('#license').html(''+ 
+        'Copyright 2022 Peter Christensen<br/>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:' + 
+        '</br>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</br>' + 
+        'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.');
         $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
     });
     authClient = new OktaAuth({
@@ -150,7 +163,7 @@ function showBoxUI(boxAccessToken) {
           }
         }
     })
-    $("#app-container").height(600);
+    $("#app-container").height(800);
     var coll = document.getElementsByClassName("collapsible");
     $(".collapsible").show();
     for (var i = 0; i < coll.length; i++) {
